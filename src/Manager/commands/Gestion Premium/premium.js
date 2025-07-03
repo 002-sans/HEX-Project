2b04c23227ac2bb4ff356084937647bcd423b7d9
Sans <002notsans@gmail.com>
2025-07-03 21:49:46 +0200

    premium

--- src/Manager/commands/Gestion Premium/premium.js
+++ src/Manager/commands/Gestion Premium/premium.js
@@ -48,7 +48,7 @@ module.exports =
 
                 const embed = {
                     title: 'Liste des clés premium',
-                    description: keys.map(key => `\`${key}\` - Expire <t:${Math.round(codes[key].expiresAt / 1000)}:R> ${codes[key].used ? `- <@${codes[key].by}>` : ''}`).join('\n'),
+                    description: keys.map(key => `\`${key}\` - Expire <t:${isNaN(codes[key].expiresAt) ? codes[key].expiresAt : Math.round(codes[key].expiresAt / 1000)}:R> ${codes[key].used ? `- <@${codes[key].by}>` : ''}`).join('\n'),
                     color: 0x000000,
                 }
 
